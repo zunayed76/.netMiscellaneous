7593cc96e12f1ae4efda4b226dc77534310efe85
Zunayed Ahmed Rafi <41657507+zunayed76@users.noreply.github.com>
2022-08-21 23:17:10 +0600

    k

---
+++ ado.net_stored_procedure_parameter/WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+
+
+
+        <%--<asp:GridView ID="GridView1" runat="server"></asp:GridView>--%>
+
+        <table style="border: 1px solid black; font-family: Arial">
+            <tr>
+                <td>Employee Name
+                </td>
+                <td>
+                    <asp:TextBox ID="txtEmployeeName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Gender
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlGender" runat="server">
+                        <asp:ListItem>Male</asp:ListItem>
+                        <asp:ListItem>Female</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>Salary
+                </td>
+                <td>
+                    <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit"
+                        OnClick="btnSubmit_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="lblMessage" runat="server"></asp:Label>
+                </td>
+            </tr>
+        </table>
+
+
+    </div>
+
+</asp:Content>
---
+++ ado.net_stored_procedure_parameter/WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Connection string captured from web.config
+            //string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            //// new connection object
+            //SqlConnection con = new SqlConnection(CS);
+            //try
+            //{
+            //    SqlCommand cmd = new SqlCommand("select count(*) from CmnBank", con);  // new sqlcommand(the command to be executed, connection object con)
+            //    con.Open(); //opens the connection to sql server
+            //    int totalRows = (int)cmd.ExecuteScalar();  // execute the command and retrieve data from database
+            //    Response.Write("Total rows are: " + totalRows.ToString());
+            //    //GridView1.DataSource = rdr;
+            //    //GridView1.DataBind();
+            //}
+            //catch(Exception eee)
+            //{
+
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+
+            //////////////SqlConnection con = new SqlConnection()
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Read the connection string from Web.Config file
+            string ConnectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                //Create the SqlCommand object
+                SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                //Specify that the SqlCommand is a stored procedure
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                //Add the input parameters to the command object
+                cmd.Parameters.AddWithValue("@Name", txtEmployeeName.Text);  //txtEmployeeName is id of the textbox
+                cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                cmd.Parameters.AddWithValue("@Salary", txtSalary.Text);
+                //cmd.Parameters.AddWithValue("@Employee", txtEmployeeName.Text); 
+
+
+
+
+                //Add the output parameter to the command object
+                SqlParameter outPutParameter = new SqlParameter();
+                outPutParameter.ParameterName = "@EmployeeId";   //name of the output parameter in stored procedure
+                outPutParameter.SqlDbType = System.Data.SqlDbType.Int;  // data type of @EmployeeID
+                outPutParameter.Direction = System.Data.ParameterDirection.Output;  //direction of the parameter is output
+                cmd.Parameters.Add(outPutParameter);
+
+                //Open the connection and execute the query
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                //Retrieve the value of the output parameter
+                string EmployeeId = outPutParameter.Value.ToString();
+                lblMessage.Text = "Employee Id = " + EmployeeId;
+            }
+        }
+
+    }       
+}
+
+
+
+
+    //code for procedure
+//Create Procedure spAddEmployee  
+//@Name nvarchar(50),  
+//@Gender nvarchar(20),  
+//@Salary int,  
+//@EmployeeId int Out  
+//as  
+//Begin  
+// Insert into tblEmployees values(@Name, @Gender, @Salary)  
+// Select @EmployeeId = SCOPE_IDENTITY()  
+//End
